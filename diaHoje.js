b3f5da0b206f5ecc508ddcd889b6249926cf9bd5
Gael Nogawa <123341163+Gael-Nogawa@users.noreply.github.com>
2023-03-03 02:02:19 -0300

    Starting task list

--- diaHoje.js
+++ diaHoje.js
@@ -10,7 +10,7 @@ function getNomeMes(numeroMes){
     const meses = ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho', 'julho', 'agosto', 'setembro', 'outubro','novembro', 'dezembro'];
     return meses[numeroMes];
 }
-
+   
 function zeroAEsquerda(num){
     return num >= 10 ? num : `0${num}`;
 }
